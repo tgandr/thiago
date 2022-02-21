5294a0d7f7bf551c843fbc85001cfac590a7eeee
tgandr <tgandr@gmail.com>
2022-02-21 13:28:45 +0000

    ajuste do card Shopping Cart

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ document.addEventListener('mouseover', (elem) => {
     background.style.backgroundImage = "url('https://revistavetshare.com.br/wp-content/uploads/2021/10/shutterstock_1915187485.jpg')"
   } if (elem.target.innerText == 'ZOOTECNIA') {
     background.style.backgroundImage = "url('https://ak.picdn.net/shutterstock/videos/3852800/thumb/1.jpg')"
+  } if (elem.target.innerText == 'PUBLICAÇÕES') {
+    background.style.backgroundImage = "url('https://www.meatpoultrynutrition.org/sites/default/files/nutrition/library.jpg')"
   } if (elem.target.innerText == 'PROGRAMAÇÃO') {
     background.style.backgroundImage = "url('https://miro.medium.com/max/1400/0*Wz93rPzLLTq1VwVW')"
   }
